46ffb10ec2761c897ba9521926cd03b144202ff4
Christopher Semard <christophersemard76@gmail.com>
2025-06-26 11:03:02 +0200

    fix users

--- backend/ms-users/src/main.ts
+++ backend/ms-users/src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
 
     await app.listen();
     console.log(`🚀 ms-users démarré sur port TCP: ${port}`);
+    console.log(`📂 Répertoire de travail: ${path.resolve(process.cwd())}`);
 }
 
 bootstrap();
